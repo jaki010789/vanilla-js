64f29f058324c6fd581d530ca2ff7b4a88901fe7
Jacqueline Garcia <you@example.com>
2023-06-20 16:59:44 -0600

    ejercicio3

--- src/ejercicio3.js
+++ src/ejercicio3.js
@@ -1,6 +1,33 @@
 // Inserte el código aquí
+let listanueva=[2,4,6];
+
+function clonarArreglo(lista) {
+    lista= [...listanueva]
+    return lista;
+
+};
+
+let objeto={
+    car: "blue",
+}
 
 
+function clonarObjeto(objeto) {
+return (clonar={...objeto});
+};
 
-// NO MODIFICAR
+function sumar(valor1, valor2) {
+return (sumar= valor1 + valor2);
+}
+
+let resultado= sumar[2,4,6,8].reduce(sumar);
+console.log(resultado);
+
+let saludar = (nombre = "desconocido") => {
+console.log ("Hola", nombre);
+};
+
+saludar("Carlos");
+
+// // NO MODIFICAR
 export { clonarArreglo, clonarObjeto, sumar, saludar };
