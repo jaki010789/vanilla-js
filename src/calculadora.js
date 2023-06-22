cea6f8414e5b31080b24452dd0c2a3126d1c720a
Jacqueline Garcia <you@example.com>
2023-06-22 15:35:31 -0600

    calculadora.js

---
+++ src/calculadora.js
@@ -0,0 +1,21 @@
+let calculadora={
+    sumar, restar, multiplicar, dividir
+}
+
+function sumar(n1,n2) { 
+    return n1 + n2;
+}
+
+function restar(n1,n2) {
+    return n1 - n2;
+}
+
+function multiplicar(n1, n2) {
+    return   n1 * n2;
+}
+
+function dividir(n1, n2) {
+    return  n1 / n2; 
+}
+
+export {calculadora};
