d80c24d8788ade932796ef6563ef3068ab8e5e4f
Jacqueline Garcia <you@example.com>
2023-06-22 10:03:50 -0600

    index.js

--- src/index.js
+++ src/index.js
@@ -11,44 +11,24 @@ function showTime() {
 }
 setInterval(showTime, 1000);
 
-let obtenerUsuarios = (resolve, reject) => {
-  const usuarios = fetch("http://localhost:3000/users");
-  if (usuarios) {
-    const user = {};
-    return resolve(user);
-  } else {
-    return reject(new Error("Error en respuesta"));
-  }
-};
-
-function crearObtenerUsuarios() {
-  return new Promise(obtenerUsuarios);
-}
-
-async function traerUsuarios() {
-  try {
-    const users = await crearObtenerUsuarios();
-    console.log("mostrar usuarios", users);
-  } catch (e) {
-    console.log(e);
-  }
-}
-traerUsuarios();
-
-const imprimirUsuarios = () => {
-  const usuarios = obtenerUsuarios();
-  usuarios
-    .then((response) => {
-      if (response.ok) {
-        return response.json();
-      } else {
-        throw new Error("Error en respuesta");
-      }
-    })
-    .then((usuarios) => {
-      console.log(usuarios);
+obtenerListausuarios();
+function obtenerListausuarios() {
+  fetch("http://localhost:3000/users")
+    .then((respuesta) => respuesta.json())
+    .then((datos) => {
+      console.log("Lista de usuarios:", datos);
     })
     .catch((error) => {
-      console.log(error);
+      console.log("Error al obtener la lista de usuarios:", error);
     });
-};
+
+  function usuarios() {
+    return new Promise(obtenerListausuarios);
+  }
+}
+
+async function datosusuario() {
+  const datosuser = await obtenerListausuarios();
+  console.log(datosuser);
+}
+datosusuario();
