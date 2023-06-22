794368080c952b05a753e1f2919883acc6359374
Jacqueline Garcia <you@example.com>
2023-06-22 15:36:16 -0600

    index.js

--- src/index.js
+++ src/index.js
@@ -1,2 +1,21 @@
 // Inserte el código aquí
+import { calculadora } from "./calculadora.js";
 
+function calcular(operacion, n1, n2) {
+  let respuesta;
+  if (operacion == "+") {
+    respuesta = calculadora.sumar(n1, n2);
+  } else if (operacion == "-") {
+    respuesta = calculadora.restar(n1, n2);
+  } else if (operacion == "*") {
+    respuesta = calculadora.multiplicar(n1, n2);
+  } else if (operacion == "/") {
+    respuesta = calculadora.dividir(n1, n2);
+  }
+
+  console.log(respuesta);
+
+  return respuesta;
+}
+
+calcular("-", 5, 2);
