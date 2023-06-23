c103c38be7a21f61101a2c938e815a21b07fbf8a
Jacqueline Garcia <you@example.com>
2023-06-23 11:53:39 -0600

    calculadora.js

---
+++ src/calculadora.js
@@ -0,0 +1,34 @@
+function suma(numero1, numero2) {
+    return numero1 + numero2;
+  }
+  
+  //Resta
+  
+  function resta(numero1, numero2) {
+    return numero1 - numero2;
+  }
+  
+  // //multiplicacion
+  
+  function multiplicar(numero1, numero2) {
+    return numero1 * numero2;
+  }
+  
+  //dividir
+  
+  function dividir(numero1, numero2) {
+    return numero1 / numero2;
+  }
+  
+  //Numeros Pares
+  function filterEvenNumbers(numbers) {
+    return numbers.filter((number) => number % 2 === 0);
+  }
+  
+  //Divisible entre 5
+  
+  function divisible5(numbers) {
+    return numbers.filter((number) => number % 5 === 0);
+  }
+  
+  export { suma, resta, multiplicar, dividir, filterEvenNumbers, divisible5 };
